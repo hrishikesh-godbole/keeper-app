03f0b94dfb105678bf89317569b2af0eff752c14
unknown <hrishikesh>
2022-10-11 19:07:01 +0530

    Updates

---
+++ precache-manifest.d0251d859ef08653c9bbbca147ec1036.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28002c366a1fd4b6536cf4c49156880c",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "f67a18edb7227313053d",
+    "url": "/keeper-app/static/js/2.3e1164d4.chunk.js"
+  },
+  {
+    "revision": "358511c25a428dadf7dd",
+    "url": "/keeper-app/static/js/main.d1822282.chunk.js"
+  },
+  {
+    "revision": "75af14cf052a362bf940",
+    "url": "/keeper-app/static/js/runtime-main.34dffe55.js"
+  }
+]);
